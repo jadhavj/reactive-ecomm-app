5deebdda8d8cd719078243b6ae4f3b02d5e485bc
jadhavj <jayesh.jadhav@xoriant.com>
2016-01-18 15:27:31 +0530

    Revised edit product.

--- public/js/add-Product.js
+++ public/js/add-Product.js
@@ -1,4 +1,4 @@
-ShoppingApp.controller('addNewItemController', ['$scope','$http', function($scope,$http){
+ShoppingApp.controller('addNewItemController', ['$scope', '$http', '$location', '$window', '$rootScope', function($scope, $http, $location, $window, $rootScope) {
 
   console.log("in addNewItemController");
 
@@ -8,6 +8,8 @@ ShoppingApp.controller('addNewItemController', ['$scope','$http', function($scop
         headers: { 'Content-Type': 'application/json; charset=UTF-8'
         		}
     }).success(function(responseData) {
+        var absUrl = $location.absUrl().split('#')[0];
+        $window.location = absUrl+"#/"+$rootScope.user.username+"/dashboard/"+$rootScope.user.role+"/";
         console.log("successfully added product");
     });   
   }
