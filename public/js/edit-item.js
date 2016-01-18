e74c6178102d468c3a6fa96b43575c533973546f
jadhavj <jayesh.jadhav@xoriant.com>
2016-01-18 16:23:59 +0530

    Fixed edit product after integration with backend.

--- public/js/edit-item.js
+++ public/js/edit-item.js
@@ -1,9 +1,17 @@
-ShoppingApp.controller("editItemController", ['$scope', 'dataTransfer', function($scope, dataTransfer){
+ShoppingApp.controller("editItemController", ['$scope', 'dataTransfer', '$http', function($scope, dataTransfer, $http){
   console.log("in editItemController");
   $scope.obj = dataTransfer.get();
 
   $scope.updateItemInfo = function(){
-    console.log("data updated to : ",$scope.obj);
+	    console.log('Edit item : ',$scope.obj);
+	    $http.post('http://localhost:9000/editProduct', $scope.obj,{
+	        headers: { 'Content-Type': 'application/json; charset=UTF-8'
+	        		}
+	    }).success(function(responseData) {
+	        var absUrl = $location.absUrl().split('#')[0];
+	        $window.location = absUrl+"#/"+$rootScope.user.username+"/dashboard/"+$rootScope.user.role+"/";
+	        console.log("successfully edited product");
+	    });   
   };
 
 }]);
