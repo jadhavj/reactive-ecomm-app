1563075f822abe501b09b0e00c9c44f3e240589e
jadhavj <jayesh.jadhav@xoriant.com>
2016-01-21 09:41:52 +0530

    Added mapping util to Mongo

--- app/utils/Mongo.java
+++ app/utils/Mongo.java
@@ -1,19 +1,28 @@
 package utils;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.MongoClient;
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Morphia;
+import org.mongodb.morphia.mapping.Mapper;
+import org.mongodb.morphia.mapping.cache.EntityCache;
 
-/**
- * Created by jujadhav on 8/1/16.
- */
 public class Mongo {
 
     private static final Morphia morphia = new Morphia();
 
     private static final Datastore datastore = morphia.createDatastore(new MongoClient(), "react-app");
-
+    
+    private static final Mapper mapper = new Mapper();
+    
+    private static final EntityCache cache = mapper.createEntityCache();
+    
     public static Datastore datastore() {
         return datastore;
     }
+    
+    public static <T> T fromDBObject(Class<T> t, BasicDBObject dbo) {
+    	return mapper.fromDBObject(t, dbo, cache);
+    }
+    
 }
