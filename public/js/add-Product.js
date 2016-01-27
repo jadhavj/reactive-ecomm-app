4904af777167942659e3aa936c3613449bb17f58
jadhavj <jayesh.jadhav@xoriant.com>
2016-01-27 12:58:28 +0530

    Add product.

--- public/js/add-Product.js
+++ public/js/add-Product.js
@@ -1,18 +1,19 @@
 ShoppingApp.controller('addNewItemController', ['$scope', '$http', 'getAbsUrl', '$rootScope','dataTransfer',function($scope, $http, getAbsUrl, $rootScope,dataTransfer) {
-	var fd = new FormData();
-    fd.append('file', $scope.myFile);
-    $scope.product.username = $rootScope.user.username;
-    fd.append('product', JSON.stringify($scope.product));
-    var uploadUrl = $rootScope.appBaseUrl + "/addProduct";
-    $http.post(uploadUrl, fd, {
-        transformRequest: angular.identity,
-        headers: {'Content-Type': undefined}
-    })
-    .success(function(){
-        console.log("successfully added product");
-        getAbsUrl.navigateTo("/"+$rootScope.user.username+"/dashboard/"+$rootScope.user.role+"/")
-    })
-    .error(function(){
-    });
-  }
+	this.addProduct = function () {
+		var fd = new FormData();
+	    fd.append('file', $scope.myFile);
+	    $scope.product.username = $rootScope.user.username;
+	    fd.append('product', JSON.stringify($scope.product));
+	    var uploadUrl = $rootScope.appBaseUrl + "/addProduct";
+	    $http.post(uploadUrl, fd, {
+	        transformRequest: angular.identity,
+	        headers: {'Content-Type': undefined}
+	    })
+	    .success(function(){
+	        console.log("successfully added product");
+	        getAbsUrl.navigateTo("/"+$rootScope.user.username+"/dashboard/"+$rootScope.user.role+"/")
+	    })
+	    .error(function(){
+	    });
+	}
 }]);
